ad24710ae7b7b9dd2506c9f1f54fbca9a2ccd4d6
Madiyar Mukushev <madiyar2000@gmail.com>
2020-10-13 15:30:12 +0600

    feat: Add «popular jobs» cards with dummy data

---
+++ app/src/main/java/kz/sdu/mentorship/JobsAdapter.java
@@ -0,0 +1,73 @@
+package kz.sdu.mentorship;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class JobsAdapter extends RecyclerView.Adapter<JobsAdapter.JobViewHolder> {
+    ArrayList<Integer> images;
+    ArrayList<Job> jobs;
+
+    public JobsAdapter(ArrayList<Integer> images, ArrayList<Job> jobs) {
+        this.images = images;
+        this.jobs = jobs;
+    }
+
+    @NonNull
+    @Override
+    public JobsAdapter.JobViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.job_list_item, parent, false);
+
+        return new JobViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JobViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return jobs.size();
+    }
+
+    class JobViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView jobNameView;
+        TextView dutyTypeView;
+        TextView salaryView;
+
+        public JobViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.card_image);
+            jobNameView = itemView.findViewById(R.id.job_name);
+            dutyTypeView = itemView.findViewById(R.id.duty_type);
+            salaryView = itemView.findViewById(R.id.job_salary);
+        }
+
+        void bind(int position) {
+            imageView.setImageResource(images.get(position));
+
+            Job job = jobs.get(position);
+            jobNameView.setText(job.getJobName());
+            dutyTypeView.setText(job.getDutyType());
+
+            @SuppressLint("DefaultLocale")
+            String formattedPriceText = String.format("$%d/h", job.getSalary());
+            salaryView.setText(formattedPriceText);
+        }
+    }
+}
--- app/src/main/java/kz/sdu/mentorship/MainActivity.java
+++ app/src/main/java/kz/sdu/mentorship/MainActivity.java
@@ -3,22 +3,63 @@ package kz.sdu.mentorship;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView popularJobsList;
+    private JobsAdapter jobsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configureBottomNavigation();
+        configureRecyclerPopularJobs();
+    }
+
+    private void configureRecyclerPopularJobs() {
+        popularJobsList = findViewById(R.id.rv_jobs);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        popularJobsList.setLayoutManager(layoutManager);
+        popularJobsList.setHasFixedSize(true);
+
+        ArrayList<Job> dummyJobsContent = generateDummyJobs();
+        ArrayList<Integer> dummyImages = generateDummyImages(dummyJobsContent.size());
+        jobsAdapter = new JobsAdapter(dummyImages, dummyJobsContent);
+
+        popularJobsList.setAdapter(jobsAdapter);
+    }
+
+    private ArrayList<Job> generateDummyJobs() {
+        return new ArrayList<>(Arrays.asList(
+            new Job("Product Designer", "Full time",50),
+            new Job("Android Developer", "Part time",69),
+            new Job("Web Developer", "Full time",89),
+            new Job("Data Scientist", "Full time",300))
+        );
+    }
+
+    private ArrayList<Integer> generateDummyImages(int dummyJobsSize) {
+        ArrayList<Integer> dummyImages = new ArrayList<>();
+        for (int i = 0; i < dummyJobsSize; i++) {
+            dummyImages.add(R.drawable.dummy_img);
+        }
+        return dummyImages;
     }
 
     private void configureBottomNavigation() {
