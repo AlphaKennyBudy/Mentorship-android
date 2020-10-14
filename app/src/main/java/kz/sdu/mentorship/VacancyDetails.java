0e4c60b3604b129f9364a076afd24581ecd974a4
Madiyar Mukushev <madiyar2000@gmail.com>
2020-10-14 13:44:28 +0600

    Add «vacancy detail» layout

---
+++ app/src/main/java/kz/sdu/mentorship/VacancyDetails.java
@@ -0,0 +1,14 @@
+package kz.sdu.mentorship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VacancyDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacancy_details);
+    }
+}
