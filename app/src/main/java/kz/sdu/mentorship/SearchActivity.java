8728032dd939a511bcdbd489cd9227ac506d84d4
Madiyar Mukushev <madiyar2000@gmail.com>
2020-10-15 14:25:28 +0600

    Improve search activity class by adding functionality

--- app/src/main/java/kz/sdu/mentorship/SearchActivity.java
+++ app/src/main/java/kz/sdu/mentorship/SearchActivity.java
@@ -2,11 +2,70 @@ package kz.sdu.mentorship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.apache.commons.text.WordUtils;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SearchActivity extends NavigationBarActivity {
 
+    static ListView categoriesListView;
+    public static String[] categories;
+
+    @SuppressLint("MissingSuperCall")
     protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, this, R.layout.activity_search);
+
+        configureCategoriesList();
+    }
+
+    private void configureCategoriesList() {
+        if (MainActivity.vacancies == null) return;
+        if (categoriesListView != null && categoriesListView.getAdapter() == null && categories != null) {
+            setAdapterToCategoriesView();
+            return;
+        }
+        fetchCategoriesList();
+        setAdapterToCategoriesView();
+        setOnClickListener();
+    }
+
+    private void fetchCategoriesList() {
+        Set<String> jobNames = new HashSet<>();
+        for (Vacancy vacancy: MainActivity.vacancies) {
+            vacancy.setJobName(WordUtils.capitalizeFully(vacancy.getJobName()));
+            jobNames.add(vacancy.getJobName());
+        }
+        categoriesListView = findViewById(R.id.categories_list);
+        categories = new String[jobNames.size()];
+        jobNames.toArray(categories);
+    }
+
+    private void setAdapterToCategoriesView() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categories);
+        categoriesListView.setAdapter(adapter);
+    }
+
+    private void setOnClickListener() {
+        categoriesListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                makeIntent(position);
+            }
+        });
+    }
+
+    private void makeIntent(int position) {
+        Intent intent = new Intent(this, VacancyBySearchActivity.class);
+        intent.putExtra(VacancyBySearchActivity.EXTRA_INTENT, position);
+        startActivity(intent);
     }
 }
