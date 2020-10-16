2489f45baffa2899354e2028511b9f5354335d40
Madiyar Mukushev <madiyar2000@gmail.com>
2020-10-16 16:08:17 +0600

    feat: Add login activity

---
+++ app/src/main/java/kz/sdu/mentorship/LoginActivity.java
@@ -0,0 +1,22 @@
+package kz.sdu.mentorship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends NavigationBarActivity {
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState, this, R.layout.activity_login);
+    }
+
+    public void onClickRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
