9f63fa857c3f200dc38881f63b16e322bc4bef9c
Madiyar Mukushev <madiyar2000@gmail.com>
2020-10-17 15:55:18 +0600

    feat: Add user class

---
+++ app/src/main/java/kz/sdu/mentorship/User.java
@@ -0,0 +1,90 @@
+package kz.sdu.mentorship;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("_id")
+    @Expose
+    String userId;
+
+    @SerializedName("username")
+    @Expose
+    String username;
+
+    @SerializedName("email")
+    @Expose
+    String email;
+
+    @SerializedName("phone_number")
+    @Expose
+    String phoneNumber;
+
+    @SerializedName("password")
+    @Expose
+    String password;
+
+    @SerializedName("first_name")
+    @Expose
+    String firstName;
+
+    @SerializedName("last_name")
+    @Expose
+    String lastName;
+
+    @SerializedName("country")
+    @Expose
+    String country;
+
+    @SerializedName("city")
+    @Expose
+    String city;
+
+    public User(String userId, String username, String email, String phoneNumber, String password, String firstName, String lastName, String country, String city) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
