3a5b858ce48022d18ecda3e158e77a8e3792e64f
Madiyar Mukushev <madiyar2000@gmail.com>
2020-11-17 20:43:58 +0600

    Add javadoc comments to home fragment

--- app/src/main/java/kz/sdu/mentorship/fragments/HomeFragment.java
+++ app/src/main/java/kz/sdu/mentorship/fragments/HomeFragment.java
@@ -63,6 +63,13 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
 //        startActivity(intent);
     }
 
+    /**
+     * Makes asynchronous request and calls functions
+     * that fill recyclerViews with content (if a fragment was not detached)
+     * and disables swipeRefresh's loading circle.
+     *
+     * @param view  the view instance for calling helper functions
+     */
     private void fetchVacancies(final View view) {
         NetworkService
                 .getInstance()
@@ -72,6 +79,7 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
                     @Override
                     public void onResponse(Call<List<Vacancy>> call, Response<List<Vacancy>> response) {
                         if (isDetached) return;
+
                         vacancies = response.body();
                         if (vacancies == null) {
                             Toast.makeText(context, getString(R.string.connection_error), Toast.LENGTH_LONG).show();
@@ -81,10 +89,6 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
                         createRecyclerPopularJobs(view);
                         createRecyclerNearbyJobs(view);
                         swipeRefreshLayout.setRefreshing(false);
-
-                        if (employers == null) {
-//                            fetchEmployers();
-                        }
                     }
 
                     @Override
@@ -95,6 +99,11 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
                 });
     }
 
+    /**
+     * Creates recycler view with popular jobs
+     *
+     * @param view  the object to pass into createRecycler function
+     */
     private void createRecyclerPopularJobs(View view) {
         if (vacancies == null || vacancies.isEmpty()) return;
         ArrayList<Integer> dummyImages = generateDummyImages(vacancies.size(), 0);
@@ -102,6 +111,11 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
                 dummyImages, R.layout.popular_job_list_item);
     }
 
+    /**
+     * Creates recycler view with nearby jobs
+     *
+     * @param view  the object to pass into createRecycler function
+     */
     private void createRecyclerNearbyJobs(View view) {
         if (vacancies == null || vacancies.isEmpty()) return;
         ArrayList<Integer> dummyImages = generateDummyImages(vacancies.size(), 1);
@@ -109,6 +123,16 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
                 dummyImages, R.layout.nearby_job_list_item);
     }
 
+    /**
+     * Gets recyclerView from a helper getRecyclerById function and sets jobsAdapter to it
+     *
+     * @param view         the object to get and configure recycler by getRecyclerById function
+     * @param viewId       the id of the view for finding it in getRecyclerById function
+     * @param orientation  the orientation of the recyclerview (horizontal, vertical)
+     * @param vacancies    list of vacancies that will be placed on viewholder
+     * @param images       list of dummy images that will be placed on viewholder
+     * @param layoutId     layout id that will be setted as viewholder layout in recycler view
+     */
     private void createRecycler(View view, int viewId, int orientation, List<Vacancy> vacancies,
                                 ArrayList<Integer> images, int layoutId) {
         jobsList = getRecyclerById(view, viewId, orientation);
@@ -116,6 +140,14 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
         jobsList.setAdapter(jobsAdapter);
     }
 
+    /**
+     * Helper function, that returns recyclerview with configured layoutManager
+     *
+     * @param view         the view instance with which we can find other views
+     * @param viewId       the id of recyclerView
+     * @param orientation  the orientation of the recyclerview (horizontal, vertical)
+     * @return             the recyclerView that we need in other functions
+     */
     private RecyclerView getRecyclerById(View view, int viewId, int orientation) {
         RecyclerView jobsList = view.findViewById(viewId);
         LinearLayoutManager layoutManager = new LinearLayoutManager(context,  orientation, false);
@@ -124,6 +156,13 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
         return jobsList;
     }
 
+    /**
+     * Generate dummy images for recyclerView
+     *
+     * @param vacanciesSize  the number of images that we need to create, we get it from vacancies list
+     * @param imageType      the type of image (0 - for horizontal recyclerView, 1 - for vertical)
+     * @return               list of integers that are referring to id of images
+     */
     private ArrayList<Integer> generateDummyImages(int vacanciesSize, int imageType) {
         ArrayList<Integer> dummyImages = new ArrayList<>();
         for (int i = 0; i < vacanciesSize; i++) {
@@ -136,6 +175,13 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
         return dummyImages;
     }
 
+    /**
+     * Configures swipeRefresh. When a user swipes,
+     * the function fetches the list of vacancies even when we have the list already
+     *
+     * @param view  the view instance that we can use for
+     *              finding other views and call other functions which requires a view as a parameter
+     */
     private void setSwipeRefresh(final View view) {
         swipeRefreshLayout = view.findViewById(R.id.swipe_refresh);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -147,6 +193,11 @@ public class HomeFragment extends Fragment implements JobsAdapter.OnJobListener 
         });
     }
 
+    /**
+     * Sets isDetaches to true, which we need to make sure that our
+     * asynchronous function (fetchVacancies) does not address elements
+     * that have already been deleted (because fragment was detached)
+     */
     @Override
     public void onDetach() {
         super.onDetach();
